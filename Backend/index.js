30fa6a5fde4b337551bc2281bc4c6f0625a4bfb4
Ghassen_latrach <latrechghassen68@gmail.com>
2024-06-15 14:40:59 +0100

    Connect to mongoDb

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,15 @@
 // Initiate the server
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => { 
+    console.log('Connected to MongoDB')
+    }).catch((err) => {
+        console.log('Failed to connect to MongoDB', err)
+    });
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
